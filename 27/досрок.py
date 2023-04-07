4686f234b65351f46d526c62831d99b87340f038
teacher <obnolga@mail.ru>
2023-04-07 13:46:16 +0300

    ууу

--- 27/досрок.py
+++ 27/досрок.py
@@ -1,14 +1,14 @@
+import collections as d
 maxx = maxxs = 0
 with open('27B_7603.txt') as f:
     n = int(f.readline())
-    sp = []
+    sp = d.deque()
     k = int(f.readline())
     for i in range(k):
         sp.append(int(f.readline()))
     for x in f:
         maxx = max(maxx, sp[0])
         maxxs = max(maxxs, maxx + int(x))
-        for i in range(len(sp) - 1):
-            sp[i] = sp[i + 1]
-        sp[-1] = int(x)
+        sp.popleft()
+        sp.append(int(x))
 print(maxxs)
